aa7190f444ddd713a5a14381fa4bd93014a4314b
Antoine Hervet <antoine@Mac-mini-de-Antoine.local>
2021-10-14 18:34:04 +0200

    initial commit

---
+++ apps/xevlabs-ng-utils-demo/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'xevlabs-ng-utils-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'xevlabs-ng-utils-demo';
+}

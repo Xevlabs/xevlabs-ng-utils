a8bf8e29baf24747b0900e7e31b75fc59189bd11
AmineYoussef <amine.youssef@xevlabs.com>
2021-10-15 14:10:43 +0100

    cleanup

--- libs/xevlabs-snackbar/src/lib/xevlabs-snackbar.module.ts
+++ libs/xevlabs-snackbar/src/lib/xevlabs-snackbar.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HotToastModule } from '@ngneat/hot-toast';
 import { TranslocoModule } from '@ngneat/transloco';
-import { SnackBarService } from './services/snack-bar/snack-bar.service';
+import { SnackBarService } from './services/snack-bar.service';
 
 @NgModule({
   imports: [CommonModule, HotToastModule, TranslocoModule],

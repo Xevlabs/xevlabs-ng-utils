cfc7a44835a2130e34cc1a75f106a3a1753e39bb
Antoine H <52413145+AntoineXevlabs@users.noreply.github.com>
2021-11-08 09:58:38 +0100

    Added confirmation modal (#13)
    * Added confirmation modal
    
    * Fixed modal class
    
    * Removed class
    
    Co-authored-by: Antoine Hervet <antoine@MacBook-Air-de-Antoine.local>

--- libs/ng-strapi-table-lib/src/lib/core/datasource/strapi.datasource.ts
+++ libs/ng-strapi-table-lib/src/lib/core/datasource/strapi.datasource.ts
@@ -1,95 +1,105 @@
-import { CollectionViewer, DataSource } from '@angular/cdk/collections';
-import { BehaviorSubject, combineLatest,Observable, of } from 'rxjs';
-import { StrapiTableService } from '../services/strapi-table/strapi-table.service';
-import { catchError, finalize, startWith, take } from 'rxjs/operators';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatSort } from '@angular/material/sort';
-import { FilterModel } from '../../models/filter.model';
+import { CollectionViewer, DataSource } from '@angular/cdk/collections'
+import { BehaviorSubject, combineLatest, Observable, of } from 'rxjs'
+import { StrapiTableService } from '../services/strapi-table/strapi-table.service'
+import { catchError, finalize, startWith, take, tap } from 'rxjs/operators'
+import { MatPaginator } from '@angular/material/paginator'
+import { MatSort } from '@angular/material/sort'
+import { FilterModel } from '../../models/filter.model'
 
 export class StrapiDatasource<T> implements DataSource<T> {
-  private entitySubject = new BehaviorSubject<T[]>([]);
-  private loadingSubject = new BehaviorSubject<boolean>(true);
-  private filters$ = new BehaviorSubject<FilterModel[]>([]);
+	private entitySubject = new BehaviorSubject<T[]>([])
+	private loadingSubject = new BehaviorSubject<boolean>(true)
+	private filters$ = new BehaviorSubject<FilterModel[]>([])
 
-  private paginator!: MatPaginator;
-  private sort!: MatSort;
+	private paginator!: MatPaginator
+	private sort!: MatSort
 
-  public numberOfEntity$ = new BehaviorSubject<number>(0);
-  public loading$ = this.loadingSubject.asObservable();
+	public numberOfEntity$ = new BehaviorSubject<number>(0)
+	public loading$ = this.loadingSubject.asObservable()
 
-  constructor(private tableService: StrapiTableService, private collectionName: string) {
-  }
+	constructor(private tableService: StrapiTableService, private collectionName: string) {
+	}
 
-  connect(collectionViewer: CollectionViewer): Observable<T[]> {
-    return this.entitySubject.asObservable();
-  }
+	disableWhileRequest<T>() {
+		return (source: Observable<T>): any => {
+			this.loadingSubject.next(true)
+			return source.pipe(tap(() => this.loadingSubject.next(false)), catchError((error) => {
+				this.loadingSubject.next(false);
+				throw Error(error)
+			}))
+		}
+	}
 
-  reload() {
-    this.loadEntities(
-        this.filters$.value,
-        this.sort.direction,
-        this.sort.active,
-        this.paginator.pageIndex,
-        this.paginator.pageSize
-    );
-  }
+	connect(collectionViewer: CollectionViewer): Observable<T[]> {
+		return this.entitySubject.asObservable()
+	}
 
-  disconnect(collectionViewer: CollectionViewer): void {
-    this.entitySubject.complete();
-    this.loadingSubject.complete();
-  }
+	reload() {
+		this.loadEntities(
+			this.filters$.value,
+			this.sort.direction,
+			this.sort.active,
+			this.paginator.pageIndex,
+			this.paginator.pageSize,
+		)
+	}
 
-  private countEntities(filters: FilterModel[]) {
-    this.tableService.count(this.collectionName, filters).pipe(take(1)).subscribe(count => {
-      this.numberOfEntity$.next(count);
-    });
-  }
+	disconnect(collectionViewer: CollectionViewer): void {
+		this.entitySubject.complete()
+		this.loadingSubject.complete()
+	}
 
-  loadEntities(filters: FilterModel[] = [],
-               sortDirection = 'asc', sortField = 'id', pageIndex = 0, pageSize = 3) {
-    this.loadingSubject.next(true);
-    this.countEntities(filters);
-    this.tableService.find<T>(this.collectionName, filters, sortDirection, sortField, pageIndex, pageSize).pipe(
-      take(1),
-      catchError(() => of([])),
-      finalize(() => this.loadingSubject.next(false))
-    )
-      .subscribe(data => this.entitySubject.next(data));
-  }
+	private countEntities(filters: FilterModel[]) {
+		this.tableService.count(this.collectionName, filters).pipe(take(1)).subscribe(count => {
+			this.numberOfEntity$.next(count)
+		})
+	}
 
-  initTable(paginator: MatPaginator, sort: MatSort, baseFilters: FilterModel[] = []) {
-    this.paginator = paginator
-    this.sort = sort
-    this.filters$.next(baseFilters);
-    this.initData();
-  }
+	loadEntities(filters: FilterModel[] = [],
+	             sortDirection = 'asc', sortField = 'id', pageIndex = 0, pageSize = 3) {
+		this.loadingSubject.next(true)
+		this.countEntities(filters)
+		this.tableService.find<T>(this.collectionName, filters, sortDirection, sortField, pageIndex, pageSize).pipe(
+				take(1),
+				catchError(() => of([])),
+				finalize(() => this.loadingSubject.next(false)),
+			)
+			.subscribe(data => this.entitySubject.next(data))
+	}
 
-  private initData() {
-    combineLatest([this.filters$, this.sort.sortChange.pipe(startWith({
-      direction: this.sort.direction,
-      active: this.sort.active
-    }))]).subscribe(() => {
-      this.paginator.pageIndex = 0;
-    });
-    combineLatest([this.paginator.page.pipe(startWith({
-      pageIndex: this.paginator.pageIndex,
-      pageSize: this.paginator.pageSize
-    })), this.sort.sortChange.pipe(startWith({
-      direction: this.sort.direction,
-      active: this.sort.active
-    })),
-      this.filters$]).subscribe(([pageEvent, sortEvent, filters]) => {
-      this.loadEntities(
-        filters,
-        sortEvent.direction,
-        sortEvent.active,
-        this.paginator.pageIndex,
-        this.paginator.pageSize
-      );
-    });
-  }
+	initTable(paginator: MatPaginator, sort: MatSort, baseFilters: FilterModel[] = []) {
+		this.paginator = paginator
+		this.sort = sort
+		this.filters$.next(baseFilters)
+		this.initData()
+	}
 
-  updateFilters(newFilters: FilterModel[]) {
-    this.filters$.next(newFilters)
-  }
+	private initData() {
+		combineLatest([this.filters$, this.sort.sortChange.pipe(startWith({
+			direction: this.sort.direction,
+			active: this.sort.active,
+		}))]).subscribe(() => {
+			this.paginator.pageIndex = 0
+		})
+		combineLatest([this.paginator.page.pipe(startWith({
+			pageIndex: this.paginator.pageIndex,
+			pageSize: this.paginator.pageSize,
+		})), this.sort.sortChange.pipe(startWith({
+			direction: this.sort.direction,
+			active: this.sort.active,
+		})),
+			this.filters$]).subscribe(([pageEvent, sortEvent, filters]) => {
+			this.loadEntities(
+				filters,
+				sortEvent.direction,
+				sortEvent.active,
+				this.paginator.pageIndex,
+				this.paginator.pageSize,
+			)
+		})
+	}
+
+	updateFilters(newFilters: FilterModel[]) {
+		this.filters$.next(newFilters)
+	}
 }
---
+++ libs/xevlabs-ui-utils/src/lib/material/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatDialogModule } from '@angular/material/dialog'
+
+
+@NgModule({
+	declarations: [],
+	imports: [
+		MatButtonModule,
+		MatIconModule,
+		MatDialogModule,
+	],
+	exports: [
+		MatButtonModule,
+		MatIconModule,
+		MatDialogModule,
+	],
+})
+export class MaterialModule {
+}

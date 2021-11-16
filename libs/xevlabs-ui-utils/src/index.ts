a98756fc7fe434d5febb7c23a2e31dc8c736a2ff
AmineYoussef <amine.youssef@xevlabs.com>
2021-11-16 14:02:14 +0100

    Update index.ts

--- libs/xevlabs-ui-utils/src/index.ts
+++ libs/xevlabs-ui-utils/src/index.ts
@@ -1,3 +1,3 @@
 export * from './lib/xevlabs-ui-utils.module';
-export * from './lib/core/custom-validators/requireMatch';
+export * from './lib/core/custom-validators/requireAutoCompleteMatch';
 export * from './lib/components/confirmation-modal/confirmation-modal.component'

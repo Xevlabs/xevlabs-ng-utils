192cbc484fbc92f70f0334866dda3ab2b0f591c6
Antoine H <52413145+AntoineXevlabs@users.noreply.github.com>
2021-12-13 08:11:57 +0100

    Fixed indents

--- libs/xevlabs-ui-utils/src/lib/components/phone-number-input/phone-number-input.component.ts
+++ libs/xevlabs-ui-utils/src/lib/components/phone-number-input/phone-number-input.component.ts
@@ -1,126 +1,126 @@
 import { Component, forwardRef, Input, OnInit } from '@angular/core'
 import {
-  AbstractControl,
-  ControlValueAccessor,
-  FormBuilder,
-  FormControl,
-  NG_VALIDATORS,
-  NG_VALUE_ACCESSOR,
-  Validators,
+    AbstractControl,
+    ControlValueAccessor,
+    FormBuilder,
+    FormControl,
+    NG_VALIDATORS,
+    NG_VALUE_ACCESSOR,
+    Validators,
 } from '@angular/forms'
 import { MatSelectChange } from '@angular/material/select'
 import { countries } from '../../core/constants/countries'
 import { PhoneNumberUtil } from 'google-libphonenumber'
 
 @Component({
-  selector: 'xevlabs-ng-utils-phone-number-input',
-  templateUrl: './phone-number-input.component.html',
-  styleUrls: ['./phone-number-input.component.scss'],
-  providers: [
-    PhoneNumberUtil,
-    {
-      provide: NG_VALUE_ACCESSOR,
-      useExisting: forwardRef(() => PhoneNumberInputComponent),
-      multi: true,
-    },
-    {
-      provide: NG_VALIDATORS,
-      useExisting: PhoneNumberInputComponent,
-      multi: true,
-    }],
+    selector: 'xevlabs-ng-utils-phone-number-input',
+    templateUrl: './phone-number-input.component.html',
+    styleUrls: ['./phone-number-input.component.scss'],
+    providers: [
+        PhoneNumberUtil,
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => PhoneNumberInputComponent),
+            multi: true,
+        },
+        {
+            provide: NG_VALIDATORS,
+            useExisting: PhoneNumberInputComponent,
+            multi: true,
+        }],
 })
 export class PhoneNumberInputComponent implements OnInit, ControlValueAccessor {
 
-  @Input() label!: string
-  countries = countries
-  phoneNumberControl!: FormControl
-  selectedCountryPhone: string = countries[0].phone.toString()
+    @Input() label!: string
+    countries = countries
+    phoneNumberControl!: FormControl
+    selectedCountryPhone: string = countries[0].phone.toString()
 
-  onChange!: (value: string) => void
-  onTouched!: () => void
+    onChange!: (value: string) => void
+    onTouched!: () => void
 
-  constructor(private formBuilder: FormBuilder, private phoneUtil: PhoneNumberUtil) {
-  }
-
-  registerOnChange(fn: (value: string) => void): void {
-    this.onChange = fn
-  }
-
-  registerOnTouched(fn: () => void): void {
-    this.onTouched = fn
-  }
-
-  get selectedCountry() {
-    return this.countries.filter((country: any) => (country.phone) == this.selectedCountryPhone)[0]
-  }
-
-  get error() {
-    return !this.getCountryCode(this.selectedCountryPhone + this.phoneNumberControl?.value).valid
-  }
-
-  phoneValidator(e: PhoneNumberInputComponent) {
-    return (control: AbstractControl) => {
-      return e.error ? { invalid: true } : null
-    }
-  }
-
-  getCountryCode(internationalNumber: string): { countryCode: string, number: string, valid: boolean } {
-    let phoneNumber
-    try {
-      phoneNumber = this.phoneUtil.parseAndKeepRawInput(internationalNumber[0] === '+' ? internationalNumber : `+${internationalNumber}`)
-      const countryCode = phoneNumber.getCountryCode()
-      const nationalNumber = phoneNumber.getNationalNumber()
-      const nationalNumberString = nationalNumber ? nationalNumber.toString() : ''
-      const validNumber = phoneNumber.hasNationalNumber() && nationalNumberString.length > 8
-      return {
-        countryCode: countryCode ? countryCode.toString() : '',
-        number: nationalNumberString,
-        valid: validNumber,
-      }
-    } catch (e) {
-      return {
-        countryCode: '',
-        number: '',
-        valid: false,
-      }
+    constructor(private formBuilder: FormBuilder, private phoneUtil: PhoneNumberUtil) {
     }
 
-  }
-
-  ngOnInit() {
-    this.phoneNumberControl = this.formBuilder.control('', [Validators.required, this.phoneValidator(this)])
-    this.phoneNumberControl.valueChanges.subscribe((value: string) => {
-      this.updatePhoneNumber(value)
-    })
-  }
-
-  setCountryPhoneCode(selection: MatSelectChange) {
-    this.selectedCountryPhone = selection.value.phone
-    this.updatePhoneNumber(this.phoneNumberControl.value)
-  }
-
-  updatePhoneNumber(phoneNumber: string) {
-    const { countryCode, number, valid } = this.getCountryCode(phoneNumber)
-    if (valid && countryCode) {
-      this.selectedCountryPhone = countryCode
-      this.phoneNumberControl.setValue(number, { emitEvent: false })
-    } else if (phoneNumber.charAt(0) === '0') {
-      phoneNumber = phoneNumber.substring(1)
+    registerOnChange(fn: (value: string) => void): void {
+        this.onChange = fn
     }
-    const internationalPhoneNumber = this.selectedCountryPhone + phoneNumber
-    this.onChange(internationalPhoneNumber)
-  }
 
-  writeValue(value: string) {
-    if (value) {
-      const { countryCode, number } = this.getCountryCode(value)
-      this.selectedCountryPhone = countryCode
-      this.phoneNumberControl.setValue(number, { emitEvent: false })
+    registerOnTouched(fn: () => void): void {
+        this.onTouched = fn
     }
-  }
 
-  validate() {
-    return this.error ? { invalid: true } : null
-  }
+    get selectedCountry() {
+        return this.countries.filter((country: any) => (country.phone)==this.selectedCountryPhone)[0]
+    }
+
+    get error() {
+        return !this.getCountryCode(this.selectedCountryPhone + this.phoneNumberControl?.value).valid
+    }
+
+    phoneValidator(e: PhoneNumberInputComponent) {
+        return (control: AbstractControl) => {
+            return e.error ? { invalid: true }:null
+        }
+    }
+
+    getCountryCode(internationalNumber: string): { countryCode: string, number: string, valid: boolean } {
+        let phoneNumber
+        try {
+            phoneNumber = this.phoneUtil.parseAndKeepRawInput(internationalNumber[0]==='+' ? internationalNumber:`+${internationalNumber}`)
+            const countryCode = phoneNumber.getCountryCode()
+            const nationalNumber = phoneNumber.getNationalNumber()
+            const nationalNumberString = nationalNumber ? nationalNumber.toString():''
+            const validNumber = phoneNumber.hasNationalNumber() && nationalNumberString.length > 8
+            return {
+                countryCode: countryCode ? countryCode.toString():'',
+                number: nationalNumberString,
+                valid: validNumber,
+            }
+        } catch (e) {
+            return {
+                countryCode: '',
+                number: '',
+                valid: false,
+            }
+        }
+
+    }
+
+    ngOnInit() {
+        this.phoneNumberControl = this.formBuilder.control('', [Validators.required, this.phoneValidator(this)])
+        this.phoneNumberControl.valueChanges.subscribe((value: string) => {
+            this.updatePhoneNumber(value)
+        })
+    }
+
+    setCountryPhoneCode(selection: MatSelectChange) {
+        this.selectedCountryPhone = selection.value.phone
+        this.updatePhoneNumber(this.phoneNumberControl.value)
+    }
+
+    updatePhoneNumber(phoneNumber: string) {
+        const { countryCode, number, valid } = this.getCountryCode(phoneNumber)
+        if (valid && countryCode) {
+            this.selectedCountryPhone = countryCode
+            this.phoneNumberControl.setValue(number, { emitEvent: false })
+        } else if (phoneNumber.charAt(0)==='0') {
+            phoneNumber = phoneNumber.substring(1)
+        }
+        const internationalPhoneNumber = this.selectedCountryPhone + phoneNumber
+        this.onChange(internationalPhoneNumber)
+    }
+
+    writeValue(value: string) {
+        if (value) {
+            const { countryCode, number } = this.getCountryCode(value)
+            this.selectedCountryPhone = countryCode
+            this.phoneNumberControl.setValue(number, { emitEvent: false })
+        }
+    }
+
+    validate() {
+        return this.error ? { invalid: true }:null
+    }
 
 }

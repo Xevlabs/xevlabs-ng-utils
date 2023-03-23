4cc7cb406647279cc77cf93c2c71ba8d93c31487
Antoine H <52413145+AntoineXevlabs@users.noreply.github.com>
2023-03-23 16:44:58 +0100

    typed for strapi v4 (#69)

--- libs/ng-strapi-table-lib/src/lib/core/services/strapi-table/strapi-table.service.ts
+++ libs/ng-strapi-table-lib/src/lib/core/services/strapi-table/strapi-table.service.ts
@@ -1,57 +1,60 @@
 import { Inject, Injectable } from '@angular/core'
 import { HttpClient, HttpParams } from '@angular/common/http'
-import { Observable } from 'rxjs'
+import { map, Observable } from 'rxjs'
 import { FilterModel } from '../../../models/filter.model'
 import { TableLibOptionsModel } from '../../../models/table-lib-options.model'
 
 @Injectable({
-	providedIn: null,
+    providedIn: null,
 })
 export class StrapiTableService {
-	private baseUrl: string
+    private baseUrl: string
 
-	constructor(private http: HttpClient,
-	            @Inject('StrapiTableLibOptions') private readonly options: TableLibOptionsModel,
-	) {
-		if (!options || !options.baseUrl) {
-			throw new Error('Error: No base url provided. Provide one by using the forRoot method of the lib')
-		}
-		this.baseUrl = options.baseUrl
-	}
+    constructor(private http: HttpClient,
+                @Inject('StrapiTableLibOptions') private readonly options: TableLibOptionsModel,
+    ) {
+        if (!options || !options.baseUrl) {
+            throw new Error('Error: No base url provided. Provide one by using the forRoot method of the lib')
+        }
+        this.baseUrl = options.baseUrl
+    }
 
-	count(collectionName: string, filters: FilterModel[]) {
-		const params = this.parseStrapiFilters(filters)
-		return this.http.get<number>(`${this.baseUrl}/${collectionName}/count?${params.toString()}`)
-	}
+    count(collectionName: string, filters: FilterModel[]) {
+        const params = this.parseStrapiFilters(filters)
+        return this.http.get<number>(`${this.baseUrl}/${collectionName}/count?${params.toString()}`)
+    }
 
-	find<T>(collectionName: string, filters: FilterModel[], sortOrder = 'asc', sortField = 'id',
-	        pageNumber = 0, pageSize = 3, locale?: string): Observable<T[]> {
-		let params = this.parseStrapiFilters(filters)
+    find<T>(collectionName: string, filters: FilterModel[], sortOrder = 'asc', sortField = 'id',
+            pageNumber = 0, pageSize = 3, locale?: string): Observable<T[]> {
+        let params = this.parseStrapiFilters(filters)
         if (locale) {
             params = params.append('_locale', locale)
         }
-		params = params.appendAll({
-			_limit: pageSize.toString(),
-			_start: (pageSize * pageNumber).toString(),
-			_sort: `${sortField}:${sortOrder.toUpperCase()
-			}`,
-		})
-		return this.http.get<T[]>(`${this.baseUrl}/${collectionName}`, {
-			params,
-		})
-	}
+        params = params.appendAll({
+            _limit: pageSize.toString(),
+            _start: (pageSize * pageNumber).toString(),
+            _sort: `${sortField}:${sortOrder.toUpperCase()
+            }`,
+        })
+        return this.http.get<{ data: T[] }>(`${this.baseUrl}/${collectionName}`, {
+            params,
+        })
+            .pipe(map(({data}: { data: T[] }) => {
+                return data
+            }))
+    }
 
-	parseStrapiFilters(filters: FilterModel[]): HttpParams {
-		let params = new HttpParams()
-		filters.forEach(filter => {
-			if (Array.isArray(filter.value)) {
-				filter.value.forEach(filterValue => {
-					params = params.append(`${filter.attribute}_${filter.type}`, filterValue.toString())
-				})
-			} else {
-				params = params.append(`${filter.attribute}_${filter.type}`, filter.value.toString())
-			}
-		})
-		return params
-	}
+    parseStrapiFilters(filters: FilterModel[]): HttpParams {
+        let params = new HttpParams()
+        filters.forEach(filter => {
+            if (Array.isArray(filter.value)) {
+                filter.value.forEach(filterValue => {
+                    params = params.append(`${filter.attribute}_${filter.type}`, filterValue.toString())
+                })
+            } else {
+                params = params.append(`${filter.attribute}_${filter.type}`, filter.value.toString())
+            }
+        })
+        return params
+    }
 }

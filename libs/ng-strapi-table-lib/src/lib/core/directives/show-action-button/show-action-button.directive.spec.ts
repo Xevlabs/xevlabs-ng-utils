c2bf9127476b96080b29afda0cdc3b7bf3065dbf
NourElHouda42 <97087208+NourElHouda42@users.noreply.github.com>
2024-03-15 14:20:37 +0100

    added showActionButton directive (#97)

---
+++ libs/ng-strapi-table-lib/src/lib/core/directives/show-action-button/show-action-button.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ShowActionButtonDirective } from './show-action-button.directive';
+
+describe('ShowActionButtonDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ShowActionButtonDirective();
+    expect(directive).toBeTruthy();
+  });
+});
